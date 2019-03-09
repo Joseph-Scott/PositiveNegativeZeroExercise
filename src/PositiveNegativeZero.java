c5d901268ddd54536c53f33764c98ed58c0137d0
Joseph Scott <jbs198585@gmail.com>
2019-03-09 18:51:33 -0500

    first check

--- src/PositiveNegativeZero.java
+++ src/PositiveNegativeZero.java
@@ -1,2 +1,27 @@
+//Write a method called checkNumber with an int parameter number.
+//
+//The method should not return any value, and it needs to print out:
+//
+//- "positive" if the parameter is > 0
+//- "negative" if the parameter is < 0
+//- "zero" if the parameter number is equal to 0
+//
+//NOTE: The checkNumber method needs to be defined as "public static" like we have been doing so far in
+//the course.class
+//
+//NOTE: Do not add a main method to your solution code!
+
+
+
 public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+        checkNumber(5);
+
+    }
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        }
+    }
 }
